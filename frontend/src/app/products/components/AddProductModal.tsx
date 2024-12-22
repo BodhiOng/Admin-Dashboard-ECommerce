d94c73d8b2c8664d63a1d0bcecf01b4af433802a
BodhiOng <bodhidharma202@gmail.com>
2024-12-23 01:47:11 +0800

    made price & input empty by default

--- frontend/src/app/products/components/AddProductModal.tsx
+++ frontend/src/app/products/components/AddProductModal.tsx
@@ -131,7 +131,7 @@ const AddProductModal: React.FC<AddProductModalProps> = ({
             <input
               type="number"
               name="price"
-              value={newProduct.price}
+              value={newProduct.price === 0 ? '' : newProduct.price}
               onChange={onInputChange}
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               placeholder="Enter price"
@@ -147,7 +147,7 @@ const AddProductModal: React.FC<AddProductModalProps> = ({
             <input
               type="number"
               name="stock"
-              value={newProduct.stock}
+              value={newProduct.stock === 0 ? '' : newProduct.stock}
               onChange={onInputChange}
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               placeholder="Enter stock quantity"
